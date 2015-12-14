20eb775127e4b4ca3156e867a0cebf4929341e2c
Naggi <nagase2@gmail.com>
2015-12-15 00:22:05 +0900

    PostGresにデータベースを変更

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,7 +1,8 @@
-DROP TABLE people IF EXISTS;
+--DROP TABLE people;
 
 CREATE TABLE people  (
-    person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
+--this value is needed to be unique and auto sequence value.
+    person_id BIGINT  NOT NULL PRIMARY KEY,
     first_name VARCHAR(20),
     last_name VARCHAR(20)
 );
